79f71a1aac7a7635a508fda3829dd3ee3c1a4e87
Liz3 <accs@21xayah.com>
2019-08-13 20:22:36 +0200

    basic link handling

--- src/main/kotlin/com/savagellc/raven/gui/Helpers.kt
+++ src/main/kotlin/com/savagellc/raven/gui/Helpers.kt
@@ -2,6 +2,8 @@ package com.savagellc.raven.gui
 
 import javafx.scene.Cursor
 import javafx.scene.Node
+import java.awt.Desktop
+import java.net.URI
 
 fun cursourOnHover(node:Node, target:Cursor = Cursor.HAND, default: Cursor = Cursor.DEFAULT ) {
     node.setOnMouseEntered {
@@ -10,4 +12,9 @@ fun cursourOnHover(node:Node, target:Cursor = Cursor.HAND, default: Cursor = Cur
     node.setOnMouseExited {
         node.scene.cursor = default
     }
+}
+fun browse(url:String) {
+  Thread {
+      Desktop.getDesktop().browse(URI(url))
+  }.start()
 }
